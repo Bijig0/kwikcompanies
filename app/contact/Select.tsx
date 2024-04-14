616e2f065e426f86e7bc904768e9e63e286fec47
Brady <bradysuryasie@gmail.com>
2024-04-14 16:43:32 +1000

    more

--- app/contact/Select.tsx
+++ app/contact/Select.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Select = (props: Props) => {
   const { options } = props;
   return (
-    <div className="relative inline-flex">
+    <div className="relative flex">
       <select className="rounded-md text-gray-800 bg-white py-3 border-gray-300 w-full font-medium px-4 rounded-lg w-full text-base font-normal leading-normal bg-white border border-gray-300 appearance-none rounded transition-colors transition-shadow">
         {options.map((option) => (
           <option key={option} value={option}>

dbb6e99cbfaa533086884d5e2b57e347ca5dd66d
Brady <bradysuryasie@gmail.com>
2024-04-14 11:40:11 +1000

    trying to integrate autocomplete google

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 import AkpagerLayout from "@layouts/AkpagerLayout";
+import useClickOutside from "@utils/useClickOutside";
+import Script from "next/script";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 import RadioButtons from "./RadioButtons";
 import Select from "./Select";
 import FormValues, { titles } from "./form";
 import { BusinessHistory } from "./individual";
+
+const GOOGLE_MAPS_API_KEY = "AIzaSyD2uwgS-JBNlmWY84ryeDwy6T_-hFn0oFg";
+
+const GOOGLE_MAPS_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;
 
 const Page = () => {
   const {
@@ -13,6 +23,61 @@ const Page = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>();
+
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    callbackName: "YOUR_CALLBACK_NAME",
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When the user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        console.log("📍 Coordinates: ", { lat, lng });
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  const ref = useClickOutside(() => {
+    // When the user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
   const onSubmit = (data: FormValues) => {
     console.log(data);
   };
@@ -22,6 +87,7 @@ const Page = () => {
 
   return (
     <AkpagerLayout>
+      <Script src={GOOGLE_MAPS_URL} strategy="beforeInteractive" />
       {/* Contact Page Start */}
       <section className="contact-page py-130 rpy-100">
         <div className="container">
@@ -136,6 +202,15 @@ const Page = () => {
                     required={false}
                     data-error="Please enter your Name"
                   />
+                  <label htmlFor="message">Address</label>
+                  <input
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Where are you going?"
+                  />
+                  {/* We can use the "status" to decide whether we should display the dropdown or not */}
+                  {status === "OK" && <ul>{renderSuggestions()}</ul>}
                   <div className="col-md-12">
                     <div className="form-group mb-0">
                       <button type="submit" className="theme-btn">

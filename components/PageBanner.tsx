34e1b7a67dcff460ac9ce8d94131dd481811faae
Brady <bradysuryasie@gmail.com>
2024-04-14 14:13:22 +1000

    fixing styling

--- components/PageBanner.tsx
+++ components/PageBanner.tsx
@@ -10,7 +10,7 @@ const PageBanner = (props: Props) => {
   const { pageTitle, pageName, titleTag } = props;
   return (
     <section
-      className="page-banner-area overlay pt-90 pb-28 rpy-120 rel z-1 bgs-cover text-center"
+      className="page-banner-area overlay pt-80 pb-[4rem] rpy-120 rel z-1 bgs-cover text-center"
       style={{ backgroundImage: "url(assets/images/backgrounds/banner.jpg)" }}
     >
       <div className="container">
@@ -23,14 +23,15 @@ const PageBanner = (props: Props) => {
               data-aos-duration={1500}
               data-aos-offset={50}
             >
-              <li className="breadcrumb-item">
+              <li className="breadcrumb-item text-lg">
                 <Link href="/">Home</Link>
               </li>
-              <li className="breadcrumb-item active">{pageName}</li>
+              <li className="breadcrumb-item active text-lg">{pageName}</li>
             </ol>
           </nav>
+          <div className="my-8"></div>
           <h1
-            className="page-title"
+            className="page-title text-4xl"
             data-aos="fade-up"
             data-aos-duration={1500}
             data-aos-offset={50}

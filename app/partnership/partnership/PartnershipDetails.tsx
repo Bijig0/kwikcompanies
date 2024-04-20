a94eb74afb5edc53f78cd87ec3ab2d26f8f13450
Brady <bradysuryasie@gmail.com>
2024-04-20 17:00:21 +1000

    got the list to show up but im so hungry rn

--- app/partnership/partnership/PartnershipDetails.tsx
+++ app/partnership/partnership/PartnershipDetails.tsx
@@ -38,121 +38,128 @@ const PartnershipDetails = () => {
     }
   );
 
+  console.log({ fields });
   return (
     <>
       {fields.map((field, index) => {
-        <PartnerShipDetailsFormPartLayout
-          key={field.id}
-          header="Sole Trader Details"
-          step={2}
-        >
-          <div onClick={() => console.log(watch("partnerDetails"))}>
-            Click Me
-          </div>
-          <h6>First Partner's Details</h6>
-
-          <div className="flex items-center gap-3 ">
-            <div className="flex-1">
-              <label htmlFor="message">Partner Type</label>
-              <PartnershipFormProvider.Select
-                name={`partnerDetails.${index}.partnerType`}
-                options={partnerTypes}
-              />
+        return (
+          <PartnerShipDetailsFormPartLayout
+            key={field.id}
+            header="Sole Trader Details"
+            step={2}
+          >
+            <div onClick={() => console.log(watch("partnerDetails"))}>
+              Click Me
             </div>
-            <div className="flex-[3_3_0%] md:flex-[2_2_0%]">
-              <label htmlFor="message">First Name</label>
-              <PartnershipFormProvider.TextInput
-                placeholder="John"
-                name={`partnerDetails.${index}.name.firstName`}
-              />
-              {errors.partnerDetails[index]?.name?.firstName && (
+            <h6>First Partner's Details</h6>
+
+            <div className="flex items-center gap-3 ">
+              <div className="flex-1">
+                <label htmlFor="message">Partner Type</label>
+                <PartnershipFormProvider.Select
+                  name={`partnerDetails.${index}.partnerType`}
+                  options={partnerTypes}
+                />
+              </div>
+              <div className="flex-[3_3_0%] md:flex-[2_2_0%]">
+                <label htmlFor="message">First Name</label>
+                <PartnershipFormProvider.TextInput
+                  placeholder="John"
+                  name={`partnerDetails.${index}.name.firstName`}
+                />
+                {/* {errors.partnerDetails[index]?.name?.firstName && (
                 <ErrorText>
                   {errors.partnerDetails[index].name.firstName.message}
                 </ErrorText>
-              )}
-            </div>
-            <div className="flex-[3_3_0%] md:flex-[2_2_0%]">
-              <label htmlFor="message">Last Name</label>
-              <PartnershipFormProvider.TextInput
-                placeholder="Smith"
-                name={`partnerDetails.${index}.name.lastName`}
-              />
-              {errors.partnerDetails[index]?.name?.lastName && (
+              )} */}
+              </div>
+              <div className="flex-[3_3_0%] md:flex-[2_2_0%]">
+                <label htmlFor="message">Last Name</label>
+                <PartnershipFormProvider.TextInput
+                  placeholder="Smith"
+                  name={`partnerDetails.${index}.name.lastName`}
+                />
+                {/* {errors.partnerDetails[index]?.name?.lastName && (
                 <ErrorText>
                   {errors.partnerDetails[index].name.lastName.message}
                 </ErrorText>
-              )}
+              )} */}
+              </div>
             </div>
-          </div>
-          <div>
-            <input type="checkbox" {...register(``)} id="otherNames.answer" />
-            <label htmlFor="otherNames.answer" className="ml-2">
-              I have been known by another name in the past
-            </label>
-          </div>
-          {watch(`partnerDetails.${index}.name.otherNames.answer`) && (
             <div>
-              <label htmlFor="message">Other Name</label>
-              <Controller
-                name={`partnerDetails.${index}.name.otherNames.otherName`}
-                control={control}
-                rules={{ required: "This field is required" }}
-                render={() => (
-                  <PartnershipFormProvider.TextInput
-                    placeholder="Placeholder Name"
-                    name={`partnerDetails.${index}.name.otherNames.otherName`}
-                  />
-                )}
+              <input
+                type="checkbox"
+                {...register(`partnerDetails.${index}.name.otherNames.answer`)}
+                id={`partnerDetails.${index}.name.otherNames.answer`}
               />
-              {errors.partnerDetails[index]?.name?.otherNames?.otherName && (
-                <ErrorText>This field is required</ErrorText>
-              )}
+              <label htmlFor="otherNames.answer" className="ml-2">
+                I have been known by another name in the past
+              </label>
             </div>
-          )}
-          <div>
-            <label htmlFor="message">Email</label>
-            <PartnershipFormProvider.TextInput
-              placeholder="john_smith@gmail.com"
-              name={`partnerDetails.${index}.email`}
-            />
-            {errors.partnerDetails[index]?.email && (
+            {watch(`partnerDetails.${index}.name.otherNames.answer`) && (
+              <div>
+                <label htmlFor="message">Other Name</label>
+                <Controller
+                  name={`partnerDetails.${index}.name.otherNames.otherName`}
+                  control={control}
+                  rules={{ required: "This field is required" }}
+                  render={() => (
+                    <PartnershipFormProvider.TextInput
+                      placeholder="Placeholder Name"
+                      name={`partnerDetails.${index}.name.otherNames.otherName`}
+                    />
+                  )}
+                />
+                {/* {errors.partnerDetails[index]?.name?.otherNames?.otherName && (
+                <ErrorText>This field is required</ErrorText>
+              )} */}
+              </div>
+            )}
+            <div>
+              <label htmlFor="message">Email</label>
+              <PartnershipFormProvider.TextInput
+                placeholder="john_smith@gmail.com"
+                name={`partnerDetails.${index}.email`}
+              />
+              {/* {errors.partnerDetails[index]?.email && (
               <ErrorText>
                 {errors.partnerDetails[index].email.message}
               </ErrorText>
-            )}
-          </div>
+            )} */}
+            </div>
 
-          <div>
-            <label htmlFor="message">Phone Number</label>
-            <PartnershipFormProvider.TextInput
-              placeholder="+61 403 057 369"
-              name={`partnerDetails.${index}.phoneNumber`}
-            />
-            {errors.partnerDetails[index]?.phoneNumber && (
+            <div>
+              <label htmlFor="message">Phone Number</label>
+              <PartnershipFormProvider.TextInput
+                placeholder="+61 403 057 369"
+                name={`partnerDetails.${index}.phoneNumber`}
+              />
+              {/* {errors.partnerDetails[index]?.phoneNumber && (
               <ErrorText>
                 {errors.partnerDetails[index].phoneNumber.message}
               </ErrorText>
-            )}
-          </div>
+            )} */}
+            </div>
 
-          <div>
-            <label htmlFor="message">Date of Birth</label>
-            {/* <PartnershipFormProvider.DatePicker name="dateOfBirth" /> */}
-          </div>
+            <div>
+              <label htmlFor="message">Date of Birth</label>
+              {/* <PartnershipFormProvider.DatePicker name="dateOfBirth" /> */}
+            </div>
 
-          <div>
-            <label htmlFor="message">Tax File Number</label>
-            <PartnershipFormProvider.TextInput
-              placeholder="123 456 789"
-              name={`partnerDetails.${index}.taxFileNumber`}
-            />
-            {errors.partnerDetails[index]?.taxFileNumber && (
+            <div>
+              <label htmlFor="message">Tax File Number</label>
+              <PartnershipFormProvider.TextInput
+                placeholder="123 456 789"
+                name={`partnerDetails.${index}.taxFileNumber`}
+              />
+              {/* {errors.partnerDetails[index]?.taxFileNumber && (
               <ErrorText>
                 {errors.partnerDetails[index].taxFileNumber.message}
               </ErrorText>
-            )}
-          </div>
-        </PartnerShipDetailsFormPartLayout>;
+            )} */}
+            </div>
+          </PartnerShipDetailsFormPartLayout>
+        );
       })}
     </>
   );
